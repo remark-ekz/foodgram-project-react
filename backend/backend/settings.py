8cad3a07ed717f8de7d9e786cd19c026ec315694
Remark Ekz <yulianlatypov@yandex.ru>
2023-03-15 13:13:43 +0500

    colorfield

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -63,6 +63,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# #     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE'),
